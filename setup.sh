83f503f81138a28580e492ca6714a09e8ef517ab
Billy Junaidi <billy.junaidi@gmail.com>
2024-02-26 02:27:38 +0800

    fix start script

--- setup.sh
+++ setup.sh
@@ -41,14 +41,14 @@ _PWD=$(pwd)
 
 docker run -it --rm \
     -p 9151:9151 \
-    -v $_PWD:/code \
-    -v $_PWD/data:/home/circleci/.local/share/nulink \
-    -e $KEYSTORE_PASSWORD \
+    -v "${_PWD}":/code \
+    -v "${_PWD}/data":/home/circleci/.local/share/nulink \
+    -e KEYSTORE_PASSWORD="${KEYSTORE_PASSWORD}" \
     nulink/nulink nulink ursula init \
-    --signer keystore:///code/keystore/$KEYSTORE_FILE \
+    --signer keystore:///code/keystore/"${KEYSTORE_FILE}" \
     --eth-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
     --network horus \
     --payment-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
     --payment-network bsc_testnet \
-    --operator-address 0x$WORKER_ACCOUNT \
+    --operator-address 0x"${WORKER_ACCOUNT}" \
     --max-gas-price 10000000000
